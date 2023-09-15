7c662374fb9e8daa7dd9648396c353b5fa69312b
deep-madhvani <deep.madhvani@csu.fullerton.edu>
2023-09-15 13:43:03 -0700

    2nd commit

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + '/dist/personal-site-angular'));
+// app.get('/*', function(req,res) {
+// res.sendFile(path.join(__dirname+
+// '/dist/personal-site-angular/index.html'));});
+// app.listen(process.env.PORT || 8080);
+
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/personal-site-angular'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/personal-site-angular/index.html'));});
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 4200;
+
+// Serve static files from the 'dist' folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Route all requests to Angular's 'index.html'
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
